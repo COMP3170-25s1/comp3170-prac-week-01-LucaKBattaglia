23cd7be61ba03ea41495594f68025313b959ba57
Luca Battaglia <gianluca.battaglia1@students.mq.edu.au>
2025-03-03 14:02:00 +1100

    Week 1 - Completion

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -7,9 +7,12 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
-  
-   if (d < 0.5) {
+   float d = distance(p, vec2(0.25, 0.25));     // calculate distance to midpoint
+  	
+  	float e = abs(p.x - p.y);
+  	float r = p.x+ p.y;
+  	
+   if (r < 1.5 && r>0.5 && e<0.5) {
       o_colour = vec4(u_colour, 1);
    }
    else {
